c943e59a7cb3f26eb82208a7e1e284ba851f144b
miurt <mariia.iurtaeva@gmail.com>
2022-12-06 02:27:56 +0100

    Fixes for faster data processing

--- src/main/java/org/acme/Search.java
+++ src/main/java/org/acme/Search.java
@@ -1,26 +1,17 @@
 package org.acme;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 import com.univocity.parsers.common.processor.BeanListProcessor;
 import com.univocity.parsers.csv.CsvParser;
@@ -28,8 +19,6 @@ import com.univocity.parsers.csv.CsvParserSettings;
 import org.springframework.stereotype.Service;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.print.attribute.standard.PrinterURI;
-import javax.swing.plaf.synth.SynthPasswordFieldUI;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -46,6 +35,7 @@ public class Search {
     private final Logger LOG = LoggerFactory.getLogger(Search.class);
     private static final int MAX_BEFORE_WRITING = 15000;
     private static final int MAX_HOLIDAY_LENGTH = 28;
+    private boolean isSplitted = false;
     private LocalDate earliestDepartureDate;
     private LocalDate latestDepartureDate;
 
@@ -72,9 +62,11 @@ public class Search {
     public List<Offer> readOffersData(String departureAirport, int countAdults, int countChildren,
             int days, String earliestDepartureDate, String latestDepartureDate)
             throws IOException {
+        if (!isSplitted){
+            splittingOffersByDays();
+        }
         SetDates(earliestDepartureDate, latestDepartureDate, days);
         List<Offer> offers = new ArrayList<Offer>();
-        //NEW CODE
         File file = new File("splitted_files/offers_" + days + "days.csv");
         try {
             FileReader reader = new FileReader(file);
@@ -119,8 +111,11 @@ public class Search {
     }
 
     public int splittingOffersByDays() {
+        if (isSplitted) return 1;
         try {
             File file = FileUtils.toFile(this.getClass().getClassLoader().getResource("offers.csv"));
+            //creating arrays to save lines from csv
+            //Using arrays instead of ArrayLists for better performance
             String[] fileBuffers = new String[MAX_HOLIDAY_LENGTH];
             Arrays.fill(fileBuffers, "");
             int[] counters = new int[MAX_HOLIDAY_LENGTH];
@@ -128,34 +123,39 @@ public class Search {
         
             FileReader reader = new FileReader(file);
             BufferedReader br = new BufferedReader(reader);
+            FileWriter[] writers = new FileWriter[MAX_HOLIDAY_LENGTH];
+            for (int i = 0; i < MAX_HOLIDAY_LENGTH; i++) {
+                writers[i] = new FileWriter("splitted_files\\offers_" + (i + 1) + "days.csv", true);
+            }
+
             br.readLine();
             String line;
             LOG.info("Start offers.csv File Splitting");
+
             while ((line = br.readLine()) != null) {
                 List<String> split = Splitter.on(',').trimResults(CharMatcher.is('"')).splitToList(line);
                 int index = getDaysBetweenDates(split.get(1).substring(0, 10), split.get(2).substring(0, 10)) - 1;
+                if(index >= MAX_HOLIDAY_LENGTH) continue; //don't read line if days > length of array
                 if (counters[index] < MAX_BEFORE_WRITING) {
                     fileBuffers[index] += line + "\n";
                     counters[index]++;
                 } else {
-                    FileWriter writer = new FileWriter("splitted_files\\offers_" + (index + 1) + "days.csv", true);
-                    writer.write(fileBuffers[index]);
+                    writers[index].write(fileBuffers[index]);
                     fileBuffers[index] = "";
                     counters[index] = 0;
-                    writer.close();
                 }
             }
-            for (int i : counters){
-                if (i != 0) {
-                    FileWriter writer = new FileWriter("splitted_files\\offers_" + (i + 1) + "days.csv", true);
-                    writer.write(fileBuffers[i]);
-                    writer.close();
+            for (int i = 0; i < MAX_HOLIDAY_LENGTH; i++){
+                if (counters[i]!= 0) {
+                    writers[i].write(fileBuffers[i]);
                 }
+                writers[i].close();
             }
             br.close();
             reader.close();
 
             LOG.info("End Offers File Splitting");
+            isSplitted = true;
             return 1;
 
         } catch (FileNotFoundException e) {
